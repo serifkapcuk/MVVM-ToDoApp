1310c0f95ce486d79ccd3f66a7117bd8fdcba197
Serif <seriffkapcuk@gmail.com>
2025-07-11 15:44:51 +0300

    init commit

---
+++ ToDoList/ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Şerif Botan Kapcuk on 11.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
