7c431d80e01e20fbd3be004a8c0a5e190f30919f
Serif <seriffkapcuk@gmail.com>
2025-07-11 17:04:05 +0300

    start with initial codes in ToDoList at Swift

---
+++ ToDoListApp/ToDoListApp/Extensions/UI/ToDoList/ToDoListViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoListViewModel.swift
+//  ToDoListApp
+//
+//  Created by Şerif Botan Kapcuk on 11.07.2025.
+//
+
+
+
+
+
+final class ToDoListViewModel {
+    
+}
---
+++ ToDoListApp/ToDoListApp/UI/ToDoList/ToDoListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ToDoListViewController.swift
+//  ToDoListApp
+//
+//  Created by Şerif Botan Kapcuk on 11.07.2025.
+//
+
+import UIKit
+
+class ToDoListViewController: UIViewController {
+    
+    var viewModel = ToDoListViewModel()
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
